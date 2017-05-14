e04bc2800bd437ec75ae7bcde7757edc9fa29de1
root <root@fr-nginx-2.dodopizzaeu.a6.internal.cloudapp.net>
2017-05-15 00:43:12 +0300

    nginx ok

--- counter.sh
+++ counter.sh
@@ -14,19 +14,22 @@ this::on_trap(){
 this::watcher(){
 	sys::var::set cnt 0
 	while :; do
-                local m5xx=$( sys::var::get cnt )
-		echo $m5xx > "/dev/shm/__5xx.metric"
+		sys::var::get cnt    > "/dev/shm/__cnt.metric"
+		sys::var::get cnt5xx > "/dev/shm/__5xx.metric"
 
                 sys::var::set cnt 0
-                sleep 3
+		sys::var::set cnt5xx 0
+
+                sleep 60	# 1 minute agregation
         done
 }
 
 threads::run_standalone this::watcher
 
-tail -n 1 -f /var/log/nginx/access.log | while IFS='' read line; do
+tail -n 1 -f /var/log/nginx/mtail.log | while IFS='' read line; do
 
-	local status=$( grep -m 1 -oP '(?<=\<status\>)(\d{3})(?=\<\/status\>)' )
-	echo status
+	nginx_resp_status=$( grep -m 1 -oP '(?<=\<status\>)(\d{3})(?=\<\/status\>)' )
+	[ $nginx_resp_status -ge 500 -a $nginx_resp_status -le 599 ] && sys::var::inc cnt5xx
 	sys::var::inc cnt
+
 done
--- httpd.sh
+++ httpd.sh
@@ -2,7 +2,7 @@
 trap httpd::on_trap EXIT
 httpd::on_trap(){ 
 	{ pkill -P $$; } 2> /dev/null
-	rm -f "/dev/shm/__5xx.metric"
+	rm -f "/dev/shm/__*.metric"
 	echo "Shutting down httpd"
 }
 
